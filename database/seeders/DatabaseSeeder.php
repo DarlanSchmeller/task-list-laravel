c086631e09324a9a908f7c4e2fec7c59007cf7e5
Darlan Rodrigues Schmeller <darlanschimeller@gmail.com>
2025-09-16 17:16:15 -0300

    Create mock data json files, add handling to insert mock data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,12 @@
 
 namespace Database\Seeders;
 
+use App\Models\ChecklistItem;
 use App\Models\Task;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class DatabaseSeeder extends Seeder
 {
@@ -23,9 +24,24 @@ class DatabaseSeeder extends Seeder
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1');
 
+
+        $taskData = json_decode(File::get(database_path('seeders/data/task.json')), true);
+        $checklistData = json_decode(File::get(database_path('seeders/data/checklist.json')), true);
+        
+        foreach ($taskData as $data) {
+            Task::create($data);
+        }
+
+        // Get task ids to use for checklist items
+        $taskIds = Task::pluck('id')->all();
+
+        foreach ($checklistData as $data) {
+            $data['task_id'] = $taskIds[array_rand($taskIds)];
+            ChecklistItem::create($data);
+        }
+
         User::factory(10)->create();
         User::factory(2)->unverified()->create();
-        Task::factory(10)->create();
 
         User::factory()->create([
             'name' => 'Test User',
